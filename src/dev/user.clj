cfa3c861368eb6a0da2d22335f85b191ae457c6c
Tony Kay <tony.kay@gmail.com>
2019-12-06 18:02:01 -0800

    initial commit

---
+++ src/dev/user.clj
@@ -0,0 +1,14 @@
+(ns user
+  (:require
+    [clojure.pprint :refer [pprint]]
+    [clojure.spec.alpha :as s]
+    [clojure.tools.namespace.repl
+     :as tools-ns
+     :refer [disable-reload! refresh clear set-refresh-dirs]]
+    [expound.alpha :as expound]
+    [taoensso.timbre :as log]))
+
+(set-refresh-dirs "src/main" "src/test" "src/dev")
+
+(alter-var-root #'s/*explain-out* (constantly expound/printer))
+
