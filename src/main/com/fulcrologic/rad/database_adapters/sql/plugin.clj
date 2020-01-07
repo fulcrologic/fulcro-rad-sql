8088a9064dbb8a2a2f9c4eae3cf98aaae8942f0d
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2020-01-07 01:18:59 +0100

    Coerce PgArray's into clojure vectors

--- src/main/com/fulcrologic/rad/database_adapters/sql/plugin.clj
+++ src/main/com/fulcrologic/rad/database_adapters/sql/plugin.clj
@@ -3,7 +3,8 @@
     [com.fulcrologic.rad.database-adapters.sql.connection :as sql.connection]
     [com.fulcrologic.rad.database-adapters.sql.migration  :as sql.migration]
     [com.fulcrologic.rad.database-adapters.sql.query      :as sql.query]
-    [com.fulcrologic.rad.database-adapters.sql.resolvers  :as sql.resolvers]))
+    [com.fulcrologic.rad.database-adapters.sql.resolvers  :as sql.resolvers]
+    [com.fulcrologic.rad.database-adapters.sql.result-set :as sql.rs]))
 
 ;; Connection
 (def stop-connection-pools!   #'sql.connection/stop-connection-pools!)
@@ -15,3 +16,5 @@
 (def where-attributes   #'sql.query/where-attributes)
 (def generate-resolvers #'sql.resolvers/generate-resolvers)
 (def save-form!         #'sql.resolvers/save-form!)
+
+(sql.rs/coerce-result-sets!)
--- src/main/com/fulcrologic/rad/database_adapters/sql/result_set.clj
+++ src/main/com/fulcrologic/rad/database_adapters/sql/result_set.clj
@@ -1,7 +1,8 @@
 (ns com.fulcrologic.rad.database-adapters.sql.result-set
   "This namespace provides some tools to work with jdbc result sets"
   (:require [next.jdbc.result-set :as jdbc.rs])
-  (:import (java.sql ResultSet ResultSetMetaData)))
+  (:import (java.sql ResultSet ResultSetMetaData Array)))
+
 
 (defn- get-column-names [^ResultSetMetaData meta opts]
   (assert (:key-fn opts) ":key-fn is required")
@@ -20,3 +21,15 @@
   (let [rsmeta (.getMetaData rs)
         cols   (get-column-names rsmeta opts)]
     (jdbc.rs/->MapResultSetBuilder rs rsmeta cols)))
+
+
+(defn coerce-result-sets!
+  "Will extend the JDBC ReadableColumn protocol to coerce common
+  values like vectors, dates and json objects to clojure
+  datastructures. This function must be called in order for RAD to
+  function properly."
+  []
+  (extend-protocol jdbc.rs/ReadableColumn
+    Array
+    (read-column-by-label [v _]   (vec (.getArray v)))
+    (read-column-by-index [v _ _] (vec (.getArray v)))))
