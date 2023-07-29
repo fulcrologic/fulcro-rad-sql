187f5e3f1fa60bea30d21604d0024f5591f2807e
CactusChaos <harrywilliams8@ymail.com>
2023-07-29 21:36:45 +1000

    Update migration.clj
    Added a sql-type-map option to force data-types

--- src/main/com/fulcrologic/rad/database_adapters/sql/migration.clj
+++ src/main/com/fulcrologic/rad/database_adapters/sql/migration.clj
@@ -30,18 +30,24 @@
    :symbol   "VARCHAR(200)"
    :uuid     "UUID"})
 
+;; Add any additional data types you require here
+(def sql-type-map
+  {:text "TEXT"})
+
 (>defn sql-type [{::attr/keys    [type]
-                  ::rad.sql/keys [max-length]}]
-  [::attr/attribute => string?]
-  (if (#{:string :password :keyword :symbol} type)
-    (if max-length
-      (str "VARCHAR(" max-length ")")
-      "VARCHAR(200)")
-    (if-let [result (get type-map type)]
-      result
-      (do
-        (log/error "Unsupported type" type)
-        "TEXT"))))
+                  ::rad.sql/keys [data-type max-length]}]
+       [::attr/attribute => string?]
+       (if-let [result (get sql-type-map data-type)]
+         result
+         (if (#{:string :password :keyword :symbol} type)
+           (if max-length
+             (str "VARCHAR(" max-length ")")
+             "VARCHAR(200)")
+           (if-let [result (get type-map type)]
+             result
+             (do
+               (log/error "Unsupported type" type)
+               "TEXT")))))
 
 (>defn new-table [table]
   [string? => map?]
